523b82daae6b997ce45453d65b6aa83da95a05c7
josemmelo <jose.melo@glintt.com>
2018-07-04 17:19:04 +0100

    Associate and deassociate button for matches suggestions

--- dashboard/src/views/ServiceDiscovery/ListApplicationsGroup.vue
+++ dashboard/src/views/ServiceDiscovery/ListApplicationsGroup.vue
@@ -41,20 +41,74 @@
         </tbody>
     </table>
 
-    <div class="row" v-if="services != null">
+    <div class="row" v-if="services != null && showing=='apis'">
         <div class="col-sm-12">
             <h4>{{ selectedGroup.Name }} - APIs</h4>
             <hr/>
-            <ListServices :services="services" :isLoggedIn="isLoggedIn" :loggedInUser="loggedInUser"/>
+
+            <table class="table">
+                <thead>
+                    <tr class="table-secondary" >
+                        <th scope="col">Name</th>
+                        <th scope="col">gAPI Path</th>
+                        <th scope="col">API Documentation</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="service in services" v-bind:key="service.Id" v-if="service.IsReachable || (service.UseGroupAttributes && service.GroupVisibility ) || loggedInUser">
+                        <td>{{ service.Name }}</td>
+                        <td>{{ service.MatchingURI }}</td>
+                        <td>{{ service.APIDocumentation }}</td>
+                        <td style="max-width: 20rem">
+                        <router-link :to="'/service-discovery/service?uri='+service.MatchingURI" 
+                            data-toggle="tooltip" title="More info" style="margin-right: 1em" >
+                            <i class="fas fa-info-circle"></i>
+                        </router-link>
+                        <button class="btn btn-danger" data-toggle="tooltip" title="Associate" @click="deassociate(service, selectedGroup)">
+                            Deassociate From App Group
+                        </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+           <!--  <ListServices :services="" :isLoggedIn="isLoggedIn" :loggedInUser="loggedInUser"/> -->
 
         </div>
     </div>
 
-    <div class="row" v-if="possibleMatches.length > 0">
+    <div class="row" v-if="possibleMatches.length > 0 && showing=='possibleMatches'">
         <div class="col-sm-12">
             <h4>{{ selectedGroup.Name }} - Possible Matches APIs</h4>
             <hr/>
-            <ListServices :services="possibleMatches" :isLoggedIn="isLoggedIn" :loggedInUser="loggedInUser"/>
+
+            <table class="table">
+                <thead>
+                    <tr class="table-secondary" >
+                        <th scope="col">Name</th>
+                        <th scope="col">gAPI Path</th>
+                        <th scope="col">API Documentation</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="service in possibleMatches" v-bind:key="service.Id" v-if="service.IsReachable || (service.UseGroupAttributes && service.GroupVisibility ) || loggedInUser">
+                        <td>{{ service.Name }}</td>
+                        <td>{{ service.MatchingURI }}</td>
+                        <td>{{ service.APIDocumentation }}</td>
+                        <td style="max-width: 20rem">
+                        <router-link :to="'/service-discovery/service?uri='+service.MatchingURI" 
+                            data-toggle="tooltip" title="More info" style="margin-right: 1em" >
+                            <i class="fas fa-info-circle"></i>
+                        </router-link>
+                        <button class="btn btn-success" data-toggle="tooltip" title="Associate" @click="associate(service, selectedGroup)">
+                            Associate to App Group
+                        </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+                
         </div>
     </div>
 
@@ -68,8 +122,7 @@ import ListServices from "@/components/service-discovery/ListServices"
 
 export default {
     mounted() {
-        this.fetchGroups()
-        this.listUngroupedApplications()
+        this.getData()
     },
     computed: {
         isLoggedIn() {
@@ -84,21 +137,27 @@ export default {
         return {
             editing: false,
             services: null,
-            selectedGroup: null
+            selectedGroup: null,
+            showing: null
         }
     },
     methods: {
+        getData() {
+            this.fetchGroups()
+            this.listUngroupedApplications()
+        },
         ...mapActions('appsGroups', [
             'fetchGroups',
             'updateGroup',
             'deleteGroup',
             'listUngroupedApplications',
-            'findPossibleMatches'
+            'findPossibleMatches',
         ]),
         showAPIs: function(appGroup) {
             this.$api.serviceDiscovery.applicationGroupById(appGroup.Id, response => {
                 this.selectedGroup = response.body
                 this.services = response.body.Services
+                this.showing='apis'
             })
         },
         showUngroupedAPIs: function() {
@@ -107,10 +166,28 @@ export default {
             }
 
             this.services = this.ungroupedApplications
+            this.showing='apis'
         },
         findMatches: function(appGroup) {
             this.selectedGroup = appGroup
             this.findPossibleMatches(appGroup)
+            this.showing='possibleMatches'
+        },
+        associate: function(s, g) {
+            this.$api.serviceDiscovery.addServiceToAppsGroup( g.Id, s.Id, response => {
+                if (response.status == 201) {
+                    this.findPossibleMatches(g)
+                    this.getData()
+                }
+            })
+        },
+        deassociate(s,g) {
+            this.$api.serviceDiscovery.deassociateServiceFromAppsGroup( g.Id, s.Id, response => {
+                if (response.status == 201) {
+                    this.getData()
+                    this.showAPIs(g)
+                }
+            })
         }
     },
     components: {
