9f22ae0fc2e76919dce6e3ceb5616c74c2d75300
José Melo <38080254+josemmelo@users.noreply.github.com>
2018-09-11 15:12:29 +0100

    Configuration refactor (#31)
    * ThirdPartyAuthentication load from other location
    
    * Configuration file documentation updated

--- api/thirdpartyauthentication/user_information.go
+++ api/thirdpartyauthentication/user_information.go
@@ -1,15 +1,15 @@
 package thirdpartyauthentication
 
 type UserTokenInformation struct {
-	Source      string `json:"source"`
-	Name        string `json:"name"`
-	Active      bool   `json:"active"`
+	Source      string
+	Name        string
+	Active      bool
 	Information string
 }
 
 type ProtectionInfo struct {
 	Header   string
 	UserInfo string
-	Cached bool
+	Cached   bool
 	Error    error
 }
