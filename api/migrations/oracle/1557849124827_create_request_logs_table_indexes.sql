580382a4afc69b14f54ac2e41165eff30c374425
José Melo <jose.melo@glintt.com>
2019-05-23 12:10:39 +0100

    Request logs indexes improved

--- api/migrations/oracle/1557849124827_create_request_logs_table_indexes.sql
+++ api/migrations/oracle/1557849124827_create_request_logs_table_indexes.sql
@@ -2,5 +2,7 @@
 -- Start of DDL Script for Table GPLATFORM.GAPI_SERVICES_HOSTS
 -- Generated 5/14/2019 4:54:05 PM from GPLATFORM@HSDEV
 
-CREATE INDEX gapi_request_logs_id_index ON gapi_request_logs (service_name, method, request_grouper_date)
+CREATE INDEX gapi_request_logs_id_index ON gapi_request_logs (
+    service_name, method, request_grouper_date, date_time, user_agent, remote_addr, status_code
+)
 
